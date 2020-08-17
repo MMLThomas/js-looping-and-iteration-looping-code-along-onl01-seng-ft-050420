16fe08858a0f3f613f3a50b812e484584e4a99b4
michael thomas <michael.thomas2014@gmail.com>
2020-08-17 19:59:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,10 +7,16 @@ function countDown(num){
 }
 
 function writeCards(gift, event){
+<<<<<<< HEAD
   let cards = []
   for (let i = 0; i < gift.length; i++) {
    cards.push(`Thank you, ${gift[i]}, for the wonderful ${event} gift!`);
   }
   return cards
   
+=======
+  for (i = 0, i < gift.length(), i++){
+    console.log(`Thank you, ${gift[i]}, for the wonderful ${event} gift!`);
+  }
+>>>>>>> 0e84738a752c7bb51671e83cb0097d48c660c704
 }
